24b7253126140f3bfe910860daf87c4ac7a73753
Goyal <gaurav.d.goyal@accenture.com>
2018-03-12 16:55:47 +0530

    Changes for Module 4 ie Custom Directives

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,20 +7,37 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
             <div>Date :{{event.date}}</div>
-            <div>Time :{{event.time}}</div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+            Time :{{event.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
             <div>Price : \${{ event.price }}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
+            <div *ngIf="event?.location">
+                <span>Location: {{event.location?.address}}</span>
                 <span class="pad-left">{{event.location.city}},{{event.location.country}}</span>
+            </div>
+            <div *ngIf="event.onLineUrl">
+            <span>Online Url:  {{event.onLineUrl}}</span>
             </div>
         </div>
     `,
     styles:[
-        `.pad-left{
+        `.black{
+            color:#000000 !important;
+        }
+        .bold{
+            font-weight:bold !important;
+        }
+        .pad-left{
             margin-left:10px;
         }
         .well div{
             color:#bbb;
+        }
+        .thumbnail{
+            min-height:210px;
         }
         `
     ]
@@ -31,5 +48,10 @@ export class EventThumbnailComponent {
     thumbnailMsg:any="This message from Child Component";
     logFoo(){
         console.log("Message from Child Component after button click");
+    }
+    getStartTimeClass(){
+        if(this.event && this.event.time==='8:00 am')
+            return ["black","bold"]
+        return ""
     }
 }
