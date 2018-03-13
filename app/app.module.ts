a98afb6ac7550c9680e38e49c16a2db18c8805b9
gauravgoyal1911 <gauravgoyal1911@Gmail.com>
2018-03-13 22:38:15 +0530

    Added Routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,18 @@ import {EventThumbnailComponent} from './events/event-thumbnail.component';
 import NavBarComponent from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import {EventDetailsComponent} from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import {RouterModule} from '@angular/router';
+import { CreateEventComponent } from './events/shared/create-event.component';
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [EventAppComponent, EventsListComponent,EventThumbnailComponent,NavBarComponent],
+    imports: [BrowserModule,RouterModule.forRoot(appRoutes)],
+    declarations: [EventAppComponent, EventsListComponent,
+        EventDetailsComponent,
+        EventThumbnailComponent,
+        NavBarComponent,
+    CreateEventComponent],
     providers:[EventService,ToastrService],
     bootstrap: [EventAppComponent]
 })
---
+++ app/routes.ts
@@ -0,0 +1,10 @@
+import { EventsListComponent } from './events/events-list.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/shared/create-event.component';
+
+export const appRoutes = [
+    {path:'events/new',component:CreateEventComponent},
+    { path: 'events', component: EventsListComponent },
+    { path: 'events/:id', component: EventDetailsComponent },
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+]
