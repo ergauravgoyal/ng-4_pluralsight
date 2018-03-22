2c9c233fde22f3198a0749858cd1305afd7211c7
Gaurav Goyal <gaurav.d.goyal@accenture.com>
2018-03-22 17:40:44 +0530

    Create Reactive Form

--- src/app/users/profile.component.ts
+++ src/app/users/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators,FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
@@ -33,33 +33,22 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
-  private firstName:FormControl;
-  private lastName:FormControl;
   constructor(private auth: AuthService, private router: Router) {
 
   }
   ngOnInit() {
-    this.firstName = new FormControl(this.auth.currentUser.firstName, Validators.required);
-    this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required);
+    let firstName = new FormControl(this.auth.currentUser.firstName);
+    let lastName = new FormControl(this.auth.currentUser.lastName);
     this.profileForm = new FormGroup({
-      firstName: this.firstName,
-      lastName: this.lastName
-    });
+      firstName: firstName,
+      lastName: lastName
+    })
   };
   saveProfile(formValues) {
-    if (this.profileForm.valid) {
-      this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['/events']);
-    };
-  };
+    this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['events']);
+  }
   cancel() {
-    this.router.navigate(['/events']);
-  }
-  validateFirstName(){
-    this.firstName.valid || this.firstName.untouched;
-  }
-  debugger;
-  validateLastName(){
-    this.lastName.valid || this.lastName.untouched
+    this.router.navigate(['events']);
   }
 }
